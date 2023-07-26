f28ceeaced88c611760ee91458d642465fca3756
marcuspang <marcuspangyuyang@gmail.com>
2023-07-26 12:31:08 +0000

    fix: subtract from balance withdrawing

--- src/RadarEditions.sol
+++ src/RadarEditions.sol
@@ -154,6 +154,7 @@ contract RadarEditions is
         if (!sent) {
             revert TransactionFailed();
         }
+        launchedEditions[editionId].balance -= amount;
 
         emit EditionBalanceWithdrawn(editionId, amount, msg.sender);
     }
@@ -186,7 +187,7 @@ contract RadarEditions is
 
     /// user methods
 
-    function mintEdition(uint256 editionId, uint256 amount) external payable {
+    function mintEdition(uint256 editionId, uint256 amount, bytes memory data) external payable {
         if (launchedEditions[editionId].status != EditionStatus.Launched) {
             revert EditionNotLaunched();
         }
@@ -194,8 +195,9 @@ contract RadarEditions is
             revert NotEnoughFunds();
         }
 
-        _mint(msg.sender, editionId, amount, bytes(""));
-        launchedEditions[editionId].balance += msg.value;
+        _mint(msg.sender, editionId, amount, data);
+
+        launchedEditions[editionId].balance += (msg.value - protocolFee);
     }
 
     function _authorizeUpgrade(address newImplementation) internal override onlyRole(UPGRADER_ROLE) {}
