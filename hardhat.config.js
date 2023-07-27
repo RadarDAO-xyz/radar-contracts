9f61c4e1d44bb2110c8dc6bc176a7fae3a230c00
Marcus Pang <marcuspangyuyang@gmail.com>
2023-07-27 22:54:22 +0200

    feat: add deploy artifacts for goerli

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,4 +13,16 @@ module.exports = {
     sources: "./src",
     tests: "./test",
   },
+  networks: {
+    "optimism-goerli": {
+      url: `https://optimism-mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY],
+      gas: 1000000,
+      gasPrice: 2000000000,
+    },
+    optimism: {
+      url: `https://optimism-goerli.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
