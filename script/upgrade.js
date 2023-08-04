f0bb1e5ff8e9c3ad4a2a86ad6f5c10d50269b6ab
marcuspang-cliniserve <marcus.pang@cliniserve.de>
2023-08-04 09:56:59 +0200

    feat: update upgrade script + upgrade goerli

--- script/upgrade.js
+++ script/upgrade.js
@@ -2,7 +2,8 @@ const { ethers, upgrades } = require("hardhat");
 
 async function main() {
   // Upgrading
-  const RADAR_EDITIONS_ADDRESS = "0xcb25E9dcF86dB259765bA7a986dF142B41414036";
+  // const RADAR_EDITIONS_ADDRESS = "0xcb25E9dcF86dB259765bA7a986dF142B41414036"; // mainnet
+  const RADAR_EDITIONS_ADDRESS = "0x2D22716eeb1F85cF79c5531B405517c07441e84B"; // testnet
 
   const RadarEditions = await ethers.getContractFactory("RadarEditions");
   await upgrades.upgradeProxy(RADAR_EDITIONS_ADDRESS, RadarEditions);
