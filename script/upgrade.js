3680c6d384e2529de4b835f72d73fe55d75c6afc
marcuspang <marcuspangyuyang@gmail.com>
2023-08-07 14:00:04 +0200

    feat: update mainnet proxy address

--- script/upgrade.js
+++ script/upgrade.js
@@ -2,7 +2,7 @@ const { ethers, upgrades } = require("hardhat");
 
 async function main() {
   // Upgrading
-  const RADAR_EDITIONS_ADDRESS = "0xcb25E9dcF86dB259765bA7a986dF142B41414036";
+  const RADAR_EDITIONS_ADDRESS = "0xdbed288027cCbE7F6746bB62c989E7C09C7c8059";
 
   const RadarEditions = await ethers.getContractFactory("RadarEditions");
   await upgrades.upgradeProxy(RADAR_EDITIONS_ADDRESS, RadarEditions);
